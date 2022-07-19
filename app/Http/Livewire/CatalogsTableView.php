587b2a77a2efbf7f2cd80da09ec44b5836005e11
lnlockly <lnlockly@gmail.com>
2022-07-19 19:45:35 +0300

    1123

--- app/Http/Livewire/CatalogsTableView.php
+++ app/Http/Livewire/CatalogsTableView.php
@@ -55,10 +55,5 @@ class CatalogsTableView extends TableView
         $catalog->update($data);
     }
 
-    protected function bulkActions()
-    {
-        return [
-            new CatalogDeleteAction(),
-        ];
-    }
+
 }
